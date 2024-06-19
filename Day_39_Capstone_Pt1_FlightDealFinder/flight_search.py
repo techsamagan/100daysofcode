80fcb9e61a63c4cbe9d96aafbd98d222bad77e5d
Samagan Nurdinov <156639202+techsamagan@users.noreply.github.com>
2024-06-18 20:02:29 -0600

    Delete Day_39_Capstone_Pt1_FlightDealFinder directory

--- Day_39_Capstone_Pt1_FlightDealFinder/flight_search.py
+++
@@ -1,5 +0,0 @@
-class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    def get_destination_API(self, city_name):
-        code = "TESTING"
-        return code
