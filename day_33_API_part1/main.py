4d05d9c3946870987dd9e0247ef1b2378bcfc396
Samagan Nurdinov <156639202+techsamagan@users.noreply.github.com>
2024-10-02 07:07:31 -0600

    Update main.py

--- day_33_API_part1/main.py
+++ day_33_API_part1/main.py
@@ -1,29 +1,26 @@
 from tkinter import *
 import requests
 
-# Function to get Kanye quote
 def get_quote():
-    response = requests.get("https://api.kanye.rest/")
+    response = requests.get(url="https://api.kanye.rest")
+    response.raise_for_status()
     data = response.json()
     quote = data["quote"]
     canvas.itemconfig(quote_text, text=quote)
 
-# Setting up the main window
 window = Tk()
 window.title("Kanye Says...")
 window.config(padx=50, pady=50)
 
-# Adding canvas with background image
-canvas = Canvas(window, width=300, height=414)
+canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Click the button for a quote", width=250, font=("Arial", 25, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanyee texts...", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
-# Adding Kanye button
 kanye_img = PhotoImage(file="kanye.png")
-kanye_button = Button(window, image=kanye_img, highlightthickness=0, command=get_quote)
+kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
-# Running the main loop
+
 window.mainloop()
